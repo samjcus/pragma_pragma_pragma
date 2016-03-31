2550bb2f31841ab77faacba3d4c722d674050f1e
Sam Cusworth <samjcus@googlemail.com>
2016-03-31 22:04:56 +0100

    add serial fortran prime finder

---
+++ primes/src/primes_serial.f90
@@ -0,0 +1,32 @@
+PROGRAM primes_serial
+  IMPLICIT NONE
+  INTEGER(kind=8) ::  i, j, Nprimes
+  REAL(kind=8) :: start_t, end_t
+  LOGICAL, ALLOCATABLE :: primes(:)
+  INTEGER, PARAMETER :: limit = 500000000
+
+  CALL cpu_time(start_t)
+
+  ! Initialise 
+  ALLOCATE(primes(limit))
+  primes=.true.
+  primes(1)=.false.
+  
+  ! Sieve of Eratosthenes
+  DO i=2,limit
+     IF (primes(i)) THEN
+        primes(i*i : limit : i) = .false.
+     END IF
+  END DO
+
+  ! Count up number of primes
+  Nprimes=0
+  DO i=1,limit
+     IF (primes(i)) Nprimes = Nprimes+1
+  END DO
+  CALL cpu_time(end_t)
+
+  WRITE(*,*) "Number of primes = ",Nprimes
+  WRITE(*,*) "Expended wall clock time = ",end_t-start_t  
+  
+END PROGRAM primes_serial
