283c7ef2aed5bf45ef80b1ed3a292ebf89727c80
samjcus <samjcus@users.noreply.github.com>
2016-08-11 14:33:50 +0100

    Improve WRITE() in hello_world_omp

--- hello_world/src/hello_world_omp.f90
+++ hello_world/src/hello_world_omp.f90
@@ -8,12 +8,13 @@ PROGRAM hello_f
   INTEGER :: nthreads
   !$OMP PARALLEL private(th_id)
   th_id = omp_get_thread_num()
-  WRITE (*,*) "Hello, World from thread", th_id
+  WRITE (*,'(A,I0)') "Hello, World from thread ", th_id
   !$OMP BARRIER
 
   IF (th_id == 0) THEN
      nthreads = omp_get_num_threads()
-     WRITE (*,*) "There are", nthreads, "threads"
+     WRITE (*,'(A,I0,A)') "There are ", nthreads, " threads"
+     WRITE (*,'(A,I6)') 'OpenMP Specification: ',openmp_version
   END IF
   !$OMP END PARALLEL
   
