023cbd22fa48cb57229093782905297585940411
Sam Cusworth <samjcus@googlemail.com>
2016-08-11 22:56:58 +0100

    add series vector matrix multiplication

---
+++ vec_mat_multi/src/vec_mat_multi_serial.f90
@@ -0,0 +1,50 @@
+PROGRAM vec_mat_multi_serial
+  IMPLICIT NONE
+  
+  REAL(kind=8) :: t_start, t_end
+  REAL(kind=8) :: temp
+  INTEGER :: i, j
+  INTEGER, PARAMETER :: len_vec=10000
+  
+  REAL(kind=8), ALLOCATABLE :: in_vector(:)
+  REAL(kind=8), ALLOCATABLE :: out_vector(:)
+  REAL(kind=8), ALLOCATABLE :: array(:,:)
+  REAL(kind=8), ALLOCATABLE :: test_vec(:)
+  
+  CALL cpu_time(t_start)
+
+  ! Allocate space for vector and array
+  ALLOCATE(in_vector(len_vec))
+  ALLOCATE(out_vector(len_vec))
+  ALLOCATE(array(len_vec,len_vec))
+  ALLOCATE(test_vec(len_vec))
+
+  ! Initialise vector
+  CALL RANDOM_NUMBER(in_vector)
+  
+  ! Initialise array
+  CALL RANDOM_NUMBER(array)
+  
+  DO i=1,len_vec
+     temp=0.0
+     DO j=1,len_vec
+        temp = temp + in_vector(j)*array(j,i)
+     END DO
+     out_vector(i)=temp
+  END DO
+
+  test_vec = MATMUL(in_vector,array)
+
+  test_vec = ABS(test_vec - out_vector)
+  WRITE (*,'(A,3e10.5)') 'Max error: ', MAXVAL(test_vec)
+  
+  DEALLOCATE(in_vector)
+  DEALLOCATE(array)
+  DEALLOCATE(out_vector)
+  DEALLOCATE(test_vec)
+  
+  CALL cpu_time(t_end)
+
+  WRITE(*,'(A,f10.5)') "Expended wall clock time = ",t_end-t_start
+
+END PROGRAM vec_mat_multi_serial
