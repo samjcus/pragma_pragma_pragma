72cea6578b4452c2f0cd363637200e20fe7e71d1
samjcus <samjcus@users.noreply.github.com>
2016-08-16 13:19:18 +0100

    add pthreads example

---
+++ hello_world/src/hello_world_pthreads.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <pthread.h>
+
+void *print_msg(void *thread_id)
+{
+  int th_id;
+  th_id = (int)thread_id;
+  printf("Hello, World from thread %d\n", th_id);
+  pthread_exit(NULL);
+}
+
+int main (int argc, char *argv[])
+{
+  int i,rc;
+  int nthreads=4;
+  pthread_t threads[nthreads];
+
+  // create threads individually
+  for(i=0; i<nthreads; i++)
+    {
+      rc = pthread_create(&threads[i], NULL, print_msg, (void *) i);
+      assert(rc == 0);
+    }
+  
+  printf("There are %d threads\n", nthreads);
+
+  // wait for each thread to complete
+  for (i=0; i<nthreads; i++)
+    {
+      // block until thread i completes
+      rc = pthread_join(threads[i], NULL);
+      printf("Thread %d has completed\n", i);
+      assert(rc == 0);
+    }
+
+  pthread_exit(NULL);
+
+  return EXIT_SUCCESS;
+}
