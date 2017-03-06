062578e7768a88e9e99cc1fffca5132f324c91e1
samjcus <samjcus@users.noreply.github.com>
2017-03-06 16:06:48 +0000

    Update pi_integration_omp.c

--- pi_integration/src/pi_integration_omp.c
+++ pi_integration/src/pi_integration_omp.c
@@ -9,25 +9,22 @@ int main (int argc, char *argv[])
   double t_start, t_end;
   
   num_steps=500000000;
-
-
   x=0;
   sum = 0.0;
   step = 1.0/(double) num_steps;
+  
   t_start = omp_get_wtime();
 #pragma omp parallel for reduction(+:sum) default(none) private(i,x) firstprivate(step, num_steps)
-  //schedule(static)
   for (i=0; i < num_steps; i++)
     {
       x = (i+0.5)*step;
       sum = sum + 4.0/(1.0+x*x);
     }
-  
   pi = step * sum;
   t_end = omp_get_wtime();
+  
   printf("Value of pi = %g\n",pi);
   printf("Expended wall clock time = %.20f\n", t_end - t_start);
   printf("num proc = %d\n", omp_get_num_procs());
-  return EXIT_SUCCESS;
-    
+  return EXIT_SUCCESS; 
 }
